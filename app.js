9b0ae44d8aff457f9c5b1fdab0d8db3c0135967b
zangetsu507 <jason@LAPTOP-VPK6A79E.localdomain>
2020-07-06 13:19:50 -0500

    cambios finales

--- app.js
+++ app.js
@@ -5,11 +5,18 @@ const port = 3000;
 
 const ESTUDIANTES = [
     {
+        id: 1,
         nombre: "Jason Arrocha",
         edad: 23,
     },
     {
+        id: 2,
         nombre: "Jose Knight",
+        edad: 17,
+    },
+    {
+        id: 3,
+        nombre: "Ramon Arias",
         edad: 17,
     }
 ];
@@ -25,12 +32,27 @@ app.get('/api/estudiantes/',(req, res) =>{
     });
 });
 
-app.get('/api/estudiantes/:indice',(req, res) =>{
-    res.json(ESTUDIANTES[req.params.indice]);
+app.get('/api/estudiantes/:id',(req, res) =>{
+    res.json(ESTUDIANTES[req.params.id]);
 });
 
 app.post('/api/estudiantes/',(req,res)=> {
     ESTUDIANTES.push(req.body);
     res.json(req.body);
+
 });
+
+app.put('/api/estudiantes/:id',(req,res)=> {
+    ESTUDIANTES[req.params.id].nombre = "Xadier Arrocha";
+    res.send(ESTUDIANTES[req.params.id]);
+
+});
+
+app.delete('/api/estudiantes/:id',(req,res)=> {
+    console.log(req.params.id);
+    ESTUDIANTES.splice(req.params.id);
+    res.send(ESTUDIANTES)
+
+});
+
 app.listen(port, () => console.log('Example app listening at http:/localhost:${port}'));
